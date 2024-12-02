331cd91fad934573518b49c894861d468dec173c
Nicolly-Silva1 <nicollysilvaj02@gmail.com>
2024-12-02 10:00:30 -0300

    Aprendi o que é uma variavel

---
+++ Aula 2/aula02.py
@@ -0,0 +1,9 @@
+print("olá, eu to com sono")
+
+nome_usuario = "Nicolly" #tipo -> str = string
+idade_usuario = 18  #tipo -> int = integer inteiro 
+altura_usuario = 1.56 #tipo -> float = flutuante numero decimal
+maior_de_idade = True #tipo -> bool = booleano
+
+print("Seja bem-vindo", nome_usuario, "sua idade é", idade_usuario, "sua altura é", altura_usuario, "você é maior de idade?", maior_de_idade)
+
